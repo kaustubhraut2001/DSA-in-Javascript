a7046edd05e765622dbddf47df4b9d8eb67d7598
kaustubhr2001@gmail.com <87487138+kaustubhraut2001@users.noreply.github.com>
2023-08-21 16:37:22 +0530

    with comment

--- 01_palendrome.js
+++ 01_palendrome.js
@@ -12,6 +12,7 @@ function palendrome(a) {
 
 const ispalndrome = (a) => {
     return a === Number(a.toString().split("").reverse().join(""));
+    //  we can alse use + infront of a.toString() to convert it into number
 
 }
 console.log(ispalndrome(1));
